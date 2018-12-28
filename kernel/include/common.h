374911a7bf2ded33ce7fd5741058b2e7177a78a3
LemonClear <46176829+LemonClear@users.noreply.github.com>
2018-12-28 11:09:05 +0800

    update structures

---
+++ kernel/include/common.h
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *       Filename:  common.h
+ *    Description:  common structures definition
+ *        Version:  1.0
+ *        Created:
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  LemonClear
+ * =====================================================================================
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+
+
+
+
+#endif
