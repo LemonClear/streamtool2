b834d832919ecef88f821a63d3c3a454039b4afd
LemonClear <46176829+LemonClear@users.noreply.github.com>
2019-01-01 15:09:23 +0800

    apache v2

--- lib/midlayer/function.cpp
+++ lib/midlayer/function.cpp
@@ -1,22 +1,20 @@
 /*
- * =====================================================================================
- *
- *       Filename:  world.cpp
- *
- *    Description:  none
- *
- *        Version:  1.0
- *        Created:  2018年12月13日 18时21分40秒
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Ace.An (wm), ashaoyan@gmail.com
- *   Organization:
- *
- * =====================================================================================
- */
-
-
+#
+# Copyright LemonClear
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
 #include<iostream>
 using namespace std;
 
