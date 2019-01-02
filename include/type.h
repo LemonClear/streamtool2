4e2c36bb86ff5e8dcffbe7795ea2758a36b779d7
LemonClear <46176829+LemonClear@users.noreply.github.com>
2019-01-03 00:49:00 +0800

    update data type.h

--- include/type.h
+++ include/type.h
@@ -19,14 +19,31 @@
 #define __TYPE_H__
 
 /*address*/
-typedef unsigned long address_t;
+typedef unsigned long address_t; //32bit or 64bit according to cpu
 
 /*register*/
-typedef unsigned long long reg64_t;
-typedef unsigned int       reg32_t;
+typedef unsigned long long reg64_t; //64bit
+typedef unsigned int       reg32_t; //32bit
 
 /*byte*/
-typedef unsigned char byte_t;
+typedef unsigned char byte_t; //8bit
+
+/*fp16*/
+typedef short int half; //16bit
+
+/*x86 masks*/
+#define BYTE_MASK  0x00000000000000FF; //8bit
+#define WORD_MASK  0x000000000000FFFF; //16bit
+#define DWORD_MASK 0x00000000FFFFFFFF; //32bit
+#define QWORD_MASK 0xFFFFFFFFFFFFFFFF; //64bit
+
+/*arm amba bus masks*/
+#define ARM_AMBA_BYTE_MASK  0x00000000000000FF; //8bit
+#define ARM_AMBA_HWORD_MASK 0x000000000000FFFF; //16bit
+#define ARM_AMBA_WORD_MASK  0x00000000FFFFFFFF; //32bit
+#define ARM_AMBA_DWORD_MASK 0xFFFFFFFFFFFFFFFF; //64bit
+
+/**/
 
 
 #endif
