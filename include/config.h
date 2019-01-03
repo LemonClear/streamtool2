5bb31fe6285f503c902def0c42baa88ffcb7e1f6
LemonClear <46176829+LemonClear@users.noreply.github.com>
2019-01-04 00:26:27 +0800

    update config.h descriptions

--- include/config.h
+++ include/config.h
@@ -18,7 +18,8 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * This file defines simulator configurations. FIXME: will replaced by kconfig
+ * This file defines simulator configurations.
+ * FIXME: will replaced by kconfig
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
