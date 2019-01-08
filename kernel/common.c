92ac41c87cd2cfd89bd9ef29750bd04731cb08b9
LemonClear <46176829+LemonClear@users.noreply.github.com>
2019-01-08 17:07:18 +0800

    update common initialization func

--- kernel/common.c
+++ kernel/common.c
@@ -26,6 +26,14 @@
 
 
 /**
+ * global variants
+ * @this_product: the whole product entrance struct
+ *
+ */
+ip *this_product = NULL;
+
+
+/**
  * parse_configs - parse the config files
  * @path:       config file path
  * @params:     parameter struct
@@ -38,13 +46,13 @@ static int parse_configs(char *path, param *params)
         FILE *stream = NULL;
 
         if (unlikely(!params)) {
-                printf("param struct absent! %s, %s, %d\n",
+                printf("ERR: param struct absent! %s, %s, %d\n",
                                 __FILE__, __func__, __LINE__);
                 goto ret_configs;
         }
 
         if (unlikely(!path)) {
-                printf("the config file absent! %s, %s, %d\n",
+                printf("ERR: the config file absent! %s, %s, %d\n",
                                 __FILE__, __func__, __LINE__);
                 goto ret_configs;
         }
@@ -69,13 +77,13 @@ static int parse_commandline(int argc, char *argv[], param *params)
         int ret = -1;
 
         if (unlikely(!params)) {
-                printf("param struct absent, please check! %s, %s, %d\n",
+                printf("WARN: param struct absent, please check! %s, %s, %d\n",
                                 __FILE__, __func__, __LINE__);
                 goto ret_commandline;
         }
 
         if (unlikely(argc < 2) || unlikely(!argv[1])) {
-                printf("no command line params, use default! %s, %s, %d\n",
+                printf("WARN: no command line params, use default! %s, %s, %d\n",
                                 __FILE__, __func__, __LINE__);
                 goto ret_commandline;
         }
@@ -90,14 +98,46 @@ ret_commandline:
 
 
 /**
- * initialization - the simulator MAIN entrace
+ * initialization - simulator init
  * @argc:       command line parameter counts
  * @argv:       command line parameters
+ * @params:     parameter struct
  *
+ * FIXME: demo.config need to be dynamic or further no-need
  */
-static int initialization()
+static int initialization(int argc, char *argv[], param *params)
 {
         int ret = -1;
+        char *path = "./demo.defconfig";
+
+        /*begin*/
+        printf("INFO: SIMULATOR initialization START!!!!! %s, %s, %d\n",
+                        __FILE__, __func__, __LINE__);
+
+        /*1.parse config files*/
+        ret = parse_configs(path, params);
+        if (unlikely(-1 == ret)) {
+                printf("ERR: parse config file failed! %s, %s, %d\n",
+                                __FILE__, __func__, __LINE__);
+                goto ret_init;
+        }
+
+        /*2.parse command line*/
+        if (unlikely(argc > 1) && unlikely(argv[1])) {
+                ret = parse_commandline(argc, argv, params);
+                if(unlikely(-1 == ret)) {
+                        printf("WARN: ignore command line params! %s, %s, %d\n",
+                                        __FILE__, __func__, __LINE__);
+                }
+                ret = 0;
+        }
+
+        /*3.init product from top*/
+
+
+        /*end*/
+        printf("INFO: SIMULATOR initialization END!!!!! %s, %s, %d\n",
+                        __FILE__, __func__, __LINE__);
 
 ret_init:
         return ret;
@@ -109,54 +149,39 @@ ret_init:
  * @argc:       command line parameter counts
  * @argv:       command line parameters
  *
- * FIXME: demo.config need to be dynamic or further no-need
  */
 int main(int argc, char *argv[])
 {
         int ret = -1;
-        char *path = "./demo.defconfig";
         param *params = NULL;
 
         /*begin*/
         printf("INFO: SIMULATOR execution START!!!!! %s, %s, %d\n",
                         __FILE__, __func__, __LINE__);
 
-        /*simulator config param struct*/
+        /*simulator parameters*/
         params = (void *)malloc(sizeof(param));
         if (unlikely(!params)) {
-                printf("params malloc failed! %s, %s, %d\n",
+                printf("ERR: params malloc failed! %s, %s, %d\n",
                                 __FILE__, __func__, __LINE__);
-                goto ret_main_err;
+                goto ret_main;
         }
         memset((void *)params, 0, sizeof(param));
 
-        /*1.parse config files*/
-        ret = parse_configs(path, params);
+        /*1.init*/
+        ret = initialization(argc, argv, params);
         if (unlikely(-1 == ret)) {
-                printf("parse config file failed! %s, %s, %d\n",
+                printf("ERR: simulator initialization failed! %s, %s, %d\n",
                                 __FILE__, __func__, __LINE__);
-                goto ret_main_err;
+                goto ret_main;
         }
 
-        /*2.parse command line*/
-        if (unlikely(argc > 1) && unlikely(argv[1])) {
-                ret = parse_commandline(argc, argv, params);
-                if(unlikely(-1 == ret)) {
-                        printf("ignore command line params! %s, %s, %d\n",
-                                        __FILE__, __func__, __LINE__);
-                }
-        }
+        /*2.poweron*/
+        c2xlayer_function(); //FIXME:
+        //poweron(this_product);
 
-        /*3.initialization*/
-        ret = initialization();
-        if (unlikely(-1 == ret)) {
-                printf("simulator initialization failed! %s, %s, %d\n",
-                                __FILE__, __func__, __LINE__);
-                goto ret_main_err;
-        }
-
-        /*4.clock trigger*/
-        c2xlayer_function();
+        /*3.poweroff*/
+        //poweroff(this_product);
 
         /*end*/
         printf("INFO: SIMULATOR execution END!!!!! %s, %s, %d\n",
@@ -167,7 +192,7 @@ int main(int argc, char *argv[])
 
         exit(EXIT_SUCCESS);
 
-ret_main_err:
+ret_main:
         if (params)
                 free(params);
 
